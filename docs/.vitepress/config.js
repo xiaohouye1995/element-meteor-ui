e550166e79d178d296b7e3e72d4f375b69eb1e74
流光 <363993481@qq.com>
2021-12-02 14:04:44 +0800

    feat: 初始化

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,53 @@
+const base = process.env.NODE_ENV === 'production' ? '/element-meteor-ui' : '';
+const { resolve } = require('path');
+
+module.exports = {
+  title: 'element-meteor-ui',
+  description: '基于 element-ui 二次封装的前端业务组件库',
+  // 扫描srcIncludes里面的 *.md文件
+  srcIncludes: ['src'],
+  alias: {
+    // 为了能在demo中正确的使用  import { X } from 'element-meteor-ui'
+    [`element-meteor-ui`]: resolve('./src'),
+  },
+  base,
+  themeConfig: {
+    // logo: '../logo.svg',
+    nav: [{ text: 'demo', link: '/math' }],
+    lang: 'zh-CN',
+    locales: {
+      '/': {
+        lang: 'zh-CN',
+        title: 'element-meteor-ui',
+        description: '基于 element-ui 二次封装的前端业务组件库',
+        label: '中文',
+        selectText: '语言',
+        nav: [{ text: '指南', link: '/' }],
+        sidebar: [
+          { text: '介绍', link: '/' },
+          { text: 'Button', link: '/components/button/' },
+        ],
+      },
+      '/en/': {
+        lang: 'en-US',
+        title: 'element-meteor-ui',
+        description: '基于 element-ui 二次封装的前端业务组件库',
+        label: 'English',
+        selectText: 'Languages',
+        nav: [{ text: 'Guide', link: '/' }],
+        sidebar: [
+          { text: 'Getting Started', link: '/en/' },
+          { text: 'Button', link: '/en/components/button/' },
+        ],
+      },
+    },
+    search: {
+      searchMaxSuggestions: 10,
+    },
+    repo: 'https://github.com/xiaohouye1995/element-meteor-ui',
+    repoLabel: 'Github',
+    lastUpdated: true,
+    prevLink: true,
+    nextLink: true,
+  },
+};
--- docs/index.zh-CN.md
+++ docs/index.zh-CN.md
@@ -1,3 +1,7 @@
+---
+title: 介绍
+---
+
 # element-meteor-ui
 
 下面简称`EMU`。
